6bd8d4bd3bb82fe36f8a117119e7759bba14c576
Juan Rubio <jua0729.jr@gmail.com>
2023-05-04 08:11:06 -0500

    creacion clase view

---
+++ MiniProyecto2FPOE/src/co/edu/univalle/miniproyecto2/view/VistaInicio.java
@@ -0,0 +1,15 @@
+package co.edu.univalle.miniproyecto2.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Juan David Rodriguez Rubio 2025435
+ */
+
+public class VistaInicio extends JFrame {
+    private JPanel jpContenido;
+    private JLabel jlblIntro;
+}
